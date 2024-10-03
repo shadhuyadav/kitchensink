5deab13f0895c85908deeb0bfc422987656bbaab
shadhuyadav <shadhu.java@gmail.com>
2024-10-03 20:50:20 +0530

    initial commit

---
+++ src/test/java/com/kitchensink/KitchensinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchensink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchensinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
